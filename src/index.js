7b94771ba4e562e81fa9053114bfa09ad16ec1a0
Elizabeth Hanson <elizabethhanson@Elizabeths-MacBook-Pro.local>
2018-06-26 14:19:31 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoListItem from './components/video_list_item';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAcOwd3QRCa5_lLE6fbCHwoj-5ZvHZr_78';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
+
+
+
+// Create a new component
+// Should produce some HTML and put it in the DOM
+
+//JSX appears to be HTML within JavaScript, but it is actually JavaScript
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0] 
+			}); // equal to this.setState({videos: videos})
+		});
+	}
+
+	render () {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
